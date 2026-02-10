761c38abefdaf879b44d4d437ec47d299bf0f32d
drabiot <tchartie@student.42angouleme.fr>
2026-02-10 14:00:47 +0100

    Normalize

--- Python-0-Starting/ex06/filterstring.py
+++ Python-0-Starting/ex06/filterstring.py
@@ -1,15 +1,17 @@
-import sys
+from sys import argv
 from ft_filter import ft_filter
 
 
 def main():
-    assert len(sys.argv) > 1 and len(sys.argv) <= 3, "AssertionError: the arguments are bad"
-    assert sys.argv[2].isdigit(), "AssertionError: the arguments are bad"
-    print([i for i in ft_filter(lambda x : len(x) == int(sys.argv[2]), sys.argv[1].split(None))])
+    assert len(argv) > 2 and len(argv) < 4
+    assert argv[2].isdigit()
+    it = int(argv[2])
+    st = argv[1].split(None)
+    print([i for i in ft_filter(lambda x: len(x) == it, st)])
 
 
 if __name__ == "__main__":
     try:
         main()
-    except AssertionError as error:
-        print(error)
+    except AssertionError:
+        print("AssertionError: the arguments are bad")
