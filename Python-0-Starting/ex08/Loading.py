13eb7123133eb8103f701a14e399c9bcf5dd0a0e
drabiot <tchartie@student.42angouleme.fr>
2026-02-10 14:51:49 +0100

    init ex08

---
+++ Python-0-Starting/ex08/Loading.py
@@ -0,0 +1,11 @@
+import os
+
+
+def ft_tqdm(lst: range) -> None:
+    """
+    Decorate an iterable object, returning an iterator which acts exactly
+    like the original iterable, but prints a dynamically updating
+    progressbar every time a value is requested.
+    """
+    
+    return()
