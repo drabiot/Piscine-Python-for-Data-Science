14826404693ebc1a24210baccbaaf89d991300f1
drabiot <tchartie@student.42angouleme.fr>
2026-02-20 10:27:48 +0100

    Fix norm

--- Python-0-Starting/ex09/ft_package/ft_package/main.py
+++ Python-0-Starting/ex09/ft_package/ft_package/main.py
@@ -1,4 +1,7 @@
 def count_in_list(liste: list, search: str) -> int:
+    """
+    return how many times a given string occur in a given list
+    """
     ret: int = 0
     for element in liste:
         if (element == search):
--- Python-0-Starting/ex09/ft_package/setup.py
+++ Python-0-Starting/ex09/ft_package/setup.py
@@ -3,7 +3,9 @@ from setuptools import setup, find_packages
 setup(
     name='ft_package',
     version='0.0.1',
+	description='A sample test package',
     packages=find_packages(),
     install_requires=[],
     author='tchartie',
+	author_email='tchartie@student.42angouleme.fr',
 )
