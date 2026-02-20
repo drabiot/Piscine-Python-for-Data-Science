2ea1422edfc79f8457c31e007ac46072e719edf4
drabiot <tchartie@student.42angouleme.fr>
2026-02-20 11:05:01 +0100

    Respect Flake8 Norm

--- Python-1-Array/ex00/give_bmi.py
+++ Python-1-Array/ex00/give_bmi.py
@@ -1,11 +1,18 @@
-import numpy
-
-
-def give_bmi(height: list[int | float], weight: list[int | float]) -> list[int | float]:
+def give_bmi(
+    height: list[int | float],
+    weight: list[int | float],
+) -> list[int | float]:
+    """
+    Return Body Mass Index's list calculated with
+    a given height list & a given wight list
+    """
     bmi: list[int | float] = []
     iteration = min(len(weight), len(height))
-    for i in range (iteration):
-        if isinstance(weight[i], (int, float)) and isinstance(height[i], (int, float)):
+    for i in range(iteration):
+        if (
+            isinstance(weight[i], (int, float))
+            and isinstance(height[i], (int, float))
+        ):
             bmi.append(weight[i] / (height[i] * height[i]))
         else:
             return (bmi)
@@ -13,6 +20,10 @@ def give_bmi(height: list[int | float], weight: list[int | float]) -> list[int |
 
 
 def apply_limit(bmi: list[int | float], limit: int) -> list[bool]:
+    """
+    Returns a boolean list if a given BMI list
+    is greater than a given value
+    """
     ret: list[bool] = []
     for element in bmi:
         if (element > limit):
